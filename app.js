779a71c4c6930466856296d5247883c2b3532547
{berline} <{webtkweb.jp@gmail.com}>
2023-01-02 14:50:13 +0900

    JSファイル修正しました。

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
-$(function () {
-    $("#addTodo").on("click", function () {
-        const inputTodo = $("input").val();
+$("#addTodo").on("click", function () {
+    const inputTodo = $("input").val();
 
-        $("#todoList").append('<li><input type="checkbox">' + inputTodo + "</li>");
-    });
+    $("#todoList").append('<li><input type="checkbox">' + inputTodo + "</li>");
+});
 
-    $(document).on("change", "input[type=checkbox]", function () {
-        if ($(this).is(":checked")) {
-            $(this).parent().css("text-decoration", "line-through");
-            $(this).parent().css("color", "#ccc");
-        } else {
-            $(this).parent().css("text-decoration", "none");
-            $(this).parent().css("color", "#000");
-        }
-    });
+$(document).on("change", "input[type=checkbox]", function () {
+    if ($(this).is(":checked")) {
+        $(this).parent().css("text-decoration", "line-through");
+        $(this).parent().css("color", "#ccc");
+    } else {
+        $(this).parent().css("text-decoration", "none");
+        $(this).parent().css("color", "#000");
+    }
 });
