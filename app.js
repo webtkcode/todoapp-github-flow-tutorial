04eae504ede433ab5785c321ca08bb8207a3cd25
{berline} <{webtkweb.jp@gmail.com}>
2023-01-02 14:30:20 +0900

    完了未完了の実装が完了しました

--- app.js
+++ app.js
@@ -4,4 +4,12 @@ $(function () {
 
         $("#todoList").append('<li><input type="checkbox">' + inputTodo + "</li>");
     });
+
+    $(document).on("change", "input[type=checkbox]", function () {
+        if ($(this).is(":checked")) {
+            $(this).parent().css("text-decoration", "line-through");
+        } else {
+            $(this).parent().css("text-decoration", "none");
+        }
+    });
 });
